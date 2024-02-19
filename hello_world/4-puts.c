49be20f069cb39f80a41efc363aa25dd6cbcdb01
simararustam <simararustam@gmail.com>
2024-02-19 05:59:07 -0800

    C - Hello, World/ Task-1

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main(void){
+/**
+ *main -Entry point
+ *Return : Always 0 (Succes)
+ */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
