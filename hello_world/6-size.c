c37c3a394da99eed0f0c521c41853c2a8a120f55
simararustam <simararustam@gmail.com>
2024-02-19 07:24:11 -0800

    C - Hello, World/ Task-1

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
- * main - This is the entry point of the code
- * Return: 0 Successful
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of a int: %lu byte(s)\n", sizeof(int));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
