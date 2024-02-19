37f8ecaebc1cac6e62671e51bc012d6425b53463
simararustam <simararustam@gmail.com>
2024-02-19 06:27:47 -0800

    C - Hello, World/ Task-1

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - This is the entry point of the code
+ *return - 0 Successful
+ */
+
+int main(void){
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
+}
