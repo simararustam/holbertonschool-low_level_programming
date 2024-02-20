1037d2c611c016a2ca4bed9a350819ab2dc8169a
simararustam <simararustam@gmail.com>
2024-02-20 07:21:51 -0800

    Task 2

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - entry
+ *Return: 0 succes
+ */
+
+int main(void)
+{
+	char alph = 'a';
+	char alph_ = 'A';
+
+	while (alph <= 'z')
+	{
+	putchar(alph);
+	alph++;
+	}
+	while (alph_ <= 'Z')
+	{
+	putchar(alph_);
+	alph_++;
+	}
+	return (0);
+}
