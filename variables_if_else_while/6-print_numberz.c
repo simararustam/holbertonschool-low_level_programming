07a2cbbbc721547b06e1c0ee55078106dd41745d
simararustam <simararustam@gmail.com>
2024-02-21 01:35:13 -0800

    Task 2

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -7,13 +7,13 @@
 
 int main(void)
 {
-int digit = 0;
+int digit = '0';
 
-while (digit < 10)
+while (digit <= '9')
 {
-printf("%d", digit);
+putchar(digit);
 digit++;
 }
-printf("\n");
+putchar('\n');
 return (0);
 }
