aa868c4b0c500d161d4f8e9ee16cd8f74bbea973
simararustam <simararustam@gmail.com>
2024-02-21 01:13:20 -0800

    Task 2

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 succes
+ */
+
+int main(void)
+{
+int digit = 0;
+
+while (digit < 10)
+{
+	printf("%d", digit);
+	digit++;
+}
+	printf("\n");
+	return (0);
+}
