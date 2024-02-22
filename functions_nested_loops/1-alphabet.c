ce6cccf1380e87f37deae92f82f8d69150ef1c03
simararustam <simararustam@gmail.com>
2024-02-22 13:30:28 +0000

    Task - 3

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,17 +2,16 @@
 
 /**
  * main - check the code
- * Return: Always 0.
- * print_alphabet: is a function
+ * Return: Always 0 
  */
 
 void print_alphabet(void)
 {
 	char a[] = "abcdefghijklmnopqrstuvwxyz";
-
+i
 	int i = 0;
 
-	while (i != '\n')
+	while (i != '\0')
 	{
 		_putchar(a[i]);
 		i++;
