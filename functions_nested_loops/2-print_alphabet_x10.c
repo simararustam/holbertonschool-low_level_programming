aaf4d17fd66c6f961f729bcff73f561dac843b42
simararustam <simararustam@gmail.com>
2024-02-22 14:02:55 +0000

    Task - 3

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_alphabet - entry point
+ * print_alphabet_x10 - entry point
  * Description: This function prints the alphabet, in lowercase
  * main - check the code
  * Return: Always 0
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char a[] = "abcdefghijklmnopqrstuvwxyz\n";
 
