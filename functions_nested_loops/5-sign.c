1e4b219066c67fbfd23022a792de4cdf1c739357
simararustam <simararustam@gmail.com>
2024-02-23 11:38:15 +0000

    Task - 3

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - entry point
+ * Description: This function prints the alphabet, in lowercase
+ * @c: entry parametr
+ * main - check the code
+ * Return: Always 0
+ */
+
+
+int print_sign(int n)
+{
+	if (n > 0){
+	return (1);
+	}
+	else if (n == 0){
+	return (0);
+	}
+	else{
+	return (-1);
+	}
+
+	return (0);
+}
