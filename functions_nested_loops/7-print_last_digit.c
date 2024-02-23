c7f0627a3de32f25a3cc862189e26d4bf3dcf2b0
simararustam <simararustam@gmail.com>
2024-02-23 13:53:46 +0000

    Task - 3

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - entry point
+ * Description: This function prints the alphabet, in lowercase
+ * @x: entry parametr
+ * main - check the code
+ * Return: Always 0
+ */
+
+int print_last_digit(int x)
+{
+	x = x % 10;
+	_putchar(x);
+	
+	return (0);
+}
