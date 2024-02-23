608ae221c065742798c6f41cb29b514b61bc872b
simararustam <simararustam@gmail.com>
2024-02-23 06:39:52 +0000

    Task - 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _islower - entry point
+ * Description: This function prints the alphabet, in lowercase
+ * @c: entry parametr
+ * main - check the code
+ * Return: Always 0
+ */
+
+int _islower(int c)
+{
+	int r = 'a';
+
+	if (_islower(r))
+	{
+	_putchar('1');
+	}
+	else
+	{
+	_putchar('0');
+	}
+
+	return (0);
+}
