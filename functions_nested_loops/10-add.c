2854c0b1a76d46c74cd7e272a9a758d4b769821c
simararustam <simararustam@gmail.com>
2024-02-24 17:01:44 +0000

    Task - 3

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - entry point
+ * Description: This function prints the alphabet, in lowercase
+ * @x: entry parametr
+ * main - check the code
+ * Return: Always 0
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
