612c249793794747beb2bd6a5d9c550457434269
simararustam <simararustam@gmail.com>
2024-02-24 21:53:42 +0000

    Task - 3

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -11,6 +11,8 @@ void print_times_table(int n)
 {
 	int i, j, k, a;
 
+	if (n >= 0 && n <= 15)
+	{
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
@@ -46,7 +48,4 @@ void print_times_table(int n)
 				_putchar((a / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
-		}
-		_putchar ('\n');
-	}
-}
+} _putchar ('\n'); } } }
