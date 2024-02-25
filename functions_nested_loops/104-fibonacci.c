b12ad5c0399999c39174a11728eb3faf76f81935
simararustam <simararustam@gmail.com>
2024-02-25 13:54:17 +0000

    Task - 3

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Return: 0 succes
+ */
+
+int main(void)
+{
+	unsigned long a = 1, b = 2, sum;
+	int i;
+
+	printf("%ld, %ld", a, b);
+
+	for (i = 1; i < 97; i++)
+	{
+		sum = a + b;
+		printf(", %ld", sum);
+
+		a = b;
+		b = sum;
+	}
+	printf("\n");
+
+	return (0);
+}
