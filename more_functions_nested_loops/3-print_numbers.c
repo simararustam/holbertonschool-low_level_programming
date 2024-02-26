22e081dcbaf094de158e4c12f2b9db51b91861b0
simararustam <simararustam@gmail.com>
2024-02-26 12:04:53 +0000

    Task - 1

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - entry point
+ * Description: This function prints the alphabet, in lowercase
+ * main - check the code
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
