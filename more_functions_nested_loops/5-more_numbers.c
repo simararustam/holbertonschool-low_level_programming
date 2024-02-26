748b6a03bed2a5261443fd3163c81c4f7adcc5e3
simararustam <simararustam@gmail.com>
2024-02-26 12:36:57 +0000

    Task - 1

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - entry point
+ * Description: This function prints the alphabet, in lowercase
+ * main - check the code
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+		for (i = 0; i < 10; i++)
+		{
+			for (j = 0; j <= 14; j++)
+			{
+				printf("%d", j);
+			}
+		_putchar('\n');
+		}
+}
