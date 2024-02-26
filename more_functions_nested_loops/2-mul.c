4106ca46ee0d538ff5e6a055719c578559ee3fa2
simararustam <simararustam@gmail.com>
2024-02-26 11:21:07 +0000

    Task - 1

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - entry point
+ * Description: This function prints the alphabet, in lowercase
+ * @a: int variable
+ * @b: second variable
+ * main - check the code
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+
+	printf("%d", sum);
+
+	return (0);
+}
