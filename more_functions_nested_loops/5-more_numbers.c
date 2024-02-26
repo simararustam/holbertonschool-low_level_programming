9dadb5f7ff388579c672833ffcb9dbc03697846a
simararustam <simararustam@gmail.com>
2024-02-26 12:40:02 +0000

    Task - 1

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,12 @@ void more_numbers(void)
 {
 	int i, j;
 
-		for (i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
 		{
-			for (j = 0; j <= 14; j++)
-			{
-				printf("%d", j);
-			}
-		_putchar('\n');
+			printf("%d", j);
 		}
+		_putchar('\n');
+	}
 }
