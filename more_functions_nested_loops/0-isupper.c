98d017ad8d970964186669f2a23e53a6fe4e7820
simararustam <simararustam@gmail.com>
2024-02-26 10:41:32 +0000

    Task - 1

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - entry point
+ * Description: This function prints the alphabet, in lowercase
+ * @c: int variable
+ * main - check the code
+ * Return: Always 0
+ */
+
+int _isupper(int c)
+{
+	
+}
