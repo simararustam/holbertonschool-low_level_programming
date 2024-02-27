3e5489c53444706073b52c2a97349d0c96256a83
simararustam <simararustam@gmail.com>
2024-02-27 13:19:21 +0000

    Task - 2

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - entry point
+ *
+ * @s: pointer
+ */
+
+void print_rev(char *s)
+{
+	int i, len, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len = i;
+		len += 1;
+	}
+	for (j = len - 1; j >= 0; j--)
+	{
+		printf("%c", s[j]);
+	}
+	printf("\n");
+}
