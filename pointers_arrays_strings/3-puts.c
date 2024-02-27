695e9321e067e5af3b31a6b028a5139ac9edd366
simararustam <simararustam@gmail.com>
2024-02-27 12:52:41 +0000

    Task - 2

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - entry point
+ * @str: pointer
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != 0)
+	{
+		printf("%c", str[i]);
+		i++;
+	}
+	printf("\n");
+}
