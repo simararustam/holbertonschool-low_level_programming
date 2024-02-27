3cd20389529f50b068d331bb6efd595cf9bd0e6f
simararustam <simararustam@gmail.com>
2024-02-27 04:39:45 +0000

    Task - 1

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_triangle - entry poin
+ * @size: variable
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+
+	for (i = 1; i <= size; i++)
+	{
+		for (k = size; k > i; k--)
+		{
+			_putchar(' ');
+		}
+		for (j = 0; j < i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
