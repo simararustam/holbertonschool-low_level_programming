d44dd6fa89247170c9fea703ae4fdf57a1ab4ac6
simararustam <simararustam@gmail.com>
2024-02-28 06:01:21 +0000

    Task - 2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *puts2 - entry point
+ *@str: pointer
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
