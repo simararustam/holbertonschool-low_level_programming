34d80a21fa46352e30ccd896a6ee01391b7c39c1
simararustam <simararustam@gmail.com>
2024-02-28 07:27:33 +0000

    Task - 2

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,15 +6,20 @@
 
 void rev_string(char *s)
 {
-	int i, lastindex, j, rev;
+	int i, lastindex, j, rev, n;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		lastindex = i;
 	}
 
+	n = lastindex / 2;
+
+	if (lastindex % 2 == 1)
+		n++;
+
 	{
-		for (j = 0; j <= lastindex / 2; j++)
+		for (j = 0; j <= n; j++)
 		{
 			rev = s[j];
 			s[j] = s[lastindex - j];
