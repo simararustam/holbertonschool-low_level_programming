0e08946834ea27fff4828e0dd6ae942e643fda99
simararustam <simararustam@gmail.com>
2024-02-28 08:59:44 +0000

    Task - 2

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -10,9 +10,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
+	while (src[i])
+    	{
+        	dest[i] = src[i];
+        	i++;
+    	}
 	return (dest);
 }
