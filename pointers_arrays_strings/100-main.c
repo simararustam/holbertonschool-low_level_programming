4d7ee80fc4a84d1f1d8e25a84aafdfb5d6e472a6
simararustam <simararustam@gmail.com>
2024-02-28 14:56:59 +0000

    Task - 2

--- pointers_arrays_strings/100-main.c
+++ pointers_arrays_strings/100-main.c
@@ -1,21 +1,30 @@
-#include "main."
+#include "main.h"
+#include <stdio.h>
+
 /**
- *_atoi - entry point
- *@s: pointer
- *Return: 0 succes
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-
-int _atoi(char *s)
+int main(void)
 {
-	int i;
+	int nb;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] >= 0 && s[i] <= 9)
-		{
-			return (s[i]);
-		}
-		else
-			return (0); 
-	}
+	nb = _atoi("98");
+	printf("%d\n", nb);
+	nb = _atoi("-402");
+	printf("%d\n", nb);
+	nb = _atoi("		  ------++++++-----+++++--98");
+	printf("%d\n", nb);
+	nb = _atoi("214748364");
+	printf("%d\n", nb);
+	nb = _atoi("0");
+	printf("%d\n", nb);
+	nb = _atoi("Suite 402");
+	printf("%d\n", nb);
+nb = _atoi("		 +	  +	-	-98 Battery Street; San Francisco, CA 94111 - USA			 ");
+	printf("%d\n", nb);
+	nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+	printf("%d\n", nb);
+	return (0);
 }
