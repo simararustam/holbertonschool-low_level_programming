063d8874fd78d815aa99282e47a0e3b4b5f878fc
simararustam <simararustam@gmail.com>
2024-02-29 11:36:49 +0000

    Task - 2

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strcat - entry point
+ *@dest: pointer dest
+ *@src: pointer src
+ *Return: 0 succes
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0, result[11];
+
+	while (dest[i] != '\0')
+	{
+		result[j] = dest[i];
+		i++;
+		j++;
+	}
+
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		result[j] = src[i];
+		i++;
+		j++;
+	}
+
+	i = 0;
+
+	while (result[i] != '\0')
+	{
+		dest[i] = result[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
