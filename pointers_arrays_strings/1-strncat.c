51c6d10663cb26987df904c1047b5e6466eda60a
simararustam <simararustam@gmail.com>
2024-02-29 15:31:18 +0000

    Task - 2

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,7 @@
 #include "main.h"
-
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 /**
  *_strncat - entry point
  *@dest: pointer
@@ -10,5 +12,42 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	
+	int i = 0, j = 0;
+	char *result = malloc(sizeof(dest) + sizeof(src));
+	char second[n];
+
+	while (dest[i] != '\0')
+	{
+		result[j] = dest[i];
+		i++;
+		j++;
+	}
+
+	i = 0;
+	for (i = 0; i < n; i++)
+	{
+		second[i] = src[i];
+	}
+	if (sizeof(src) < n)
+	{
+		second[i] = '\0';
+	}
+
+	i = 0;
+	while (second[i] != '\0')
+	{
+		result[j] = second[i];
+		i++;
+		j++;
+	}
+
+	i = 0;
+	while (result[i] != '\0')
+	{
+		dest[i] = result[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }
