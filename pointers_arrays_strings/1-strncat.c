d110b0e32baf2070126b18154f02a4bad8dfc683
simararustam <simararustam@gmail.com>
2024-02-29 14:51:26 +0000

    Task - 2

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_strncat - entry point
+ *@dest: pointer
+ *@src: pinter
+ *@n: integer
+ *Return: entry point
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	
+}
