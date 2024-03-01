70e07da0c7dee0d32b454dfdd387d2858e9bdeb8
simararustam <simararustam@gmail.com>
2024-03-01 15:26:34 +0000

    Task - 2

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int a = *s1 - *s2;
+	char dif = 0;
+	int i = 0;
 
-	return (a);
+	while (*(s1 + i) != 0 || *(s2 + i) != 0)
+	{
+		dif = s1[i] - s2[i];
+		if (dif != 0)
+			return (dif);
+		i++;
+	}
+	return (0);
 }
