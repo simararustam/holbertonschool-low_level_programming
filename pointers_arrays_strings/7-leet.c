fd956b1943b460036af038488df5a72bb0c8ccaf
simararustam <simararustam@gmail.com>
2024-03-02 19:39:18 +0000

    Task - 2

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -4,10 +4,23 @@
 /**
  *leet - entry point
  *Return: succes
- *@a - pointer param
+ *@s: pointer param
  */
 
-char *leet(char *a)
+char *leet(char *s)
 {
+	char a[] = { 'a', 'e', 'o', 't', 'l' };
+	char n[] = { 4, 3, 0, 7, 1 };
+	int i = 0, j;
 
+	while (s[i] != 0)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == a[j] || s[i] == a[j] - 32)
+				s[i] = n[j] + '0';
+		}
+		i++;
+	}
+	return (s);
 }
