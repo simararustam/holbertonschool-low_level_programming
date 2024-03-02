860f0f716c294d436f2c662058c17dd52e73961a
simararustam <simararustam@gmail.com>
2024-03-02 18:26:06 +0000

    Task - 2

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -7,7 +7,17 @@
  * @a: pointer
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *a)
 {
+	int i = 0;
 
+	while (a[i] != 0)
+	{
+		if (a[i] >= 97 && a[i] <= 122)
+		{
+			a[i] = a[i] - 32;
+		}
+		i++;
+	}
+	return (a);
 }
