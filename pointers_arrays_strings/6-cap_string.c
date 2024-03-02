09a943d586da679423ff6b20eaaf117721472549
simararustam <simararustam@gmail.com>
2024-03-02 18:48:55 +0000

    Task - 2

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *cap_string - entry point                                                                                           *@a: pointer                                                                                                        *Return: 0 succes                                                                                                   */
+
+char *cap_string(char *)
+{
+	
+}
