a51b212107f68f8e429ca6b6cf7612b606a9cdca
simararustam <simararustam@gmail.com>
2024-03-02 16:05:46 +0000

    Task - 2

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * reverse_array - entry point
@@ -9,5 +10,13 @@
 
 void reverse_array(int *a, int n)
 {
-     
+	int i;
+	char *result = malloc(sizeof(a));
+
+	for (i = 0; i < n / 2; i++)
+	{
+		result[i] = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = result[i];
+	}
 }
