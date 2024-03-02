a77bf437f0386d9ff95f1e93a0199cf7f42fd414
simararustam <simararustam@gmail.com>
2024-03-02 14:23:55 +0000

    Task - 2

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - entry point
+ * @a: integer parametr
+ * @n: ineteger parametr
+ */
+
+void reverse_array(int *a, int n)
+{
+	    
+}
