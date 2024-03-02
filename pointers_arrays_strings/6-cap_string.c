93d9dccdd6a248f13eadc36ab2c876d3765ddc74
simararustam <simararustam@gmail.com>
2024-03-02 19:12:47 +0000

    Task - 2

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
- *cap_string - entry point                                                                                           *@a: pointer                                                                                                        *Return: 0 succes                                                                                                   */
-
-char *cap_string(char *)
+ *cap_string - main point
+ *@a: pointer
+ *Return: 0 succes
+ */
+char *cap_string(char *a)
 {
-	
+	int i = 0;
+
+	while (a[i] != 0)
+	{
+		if (a[i] >= 97 && a[i] <= 122 && a[i - 1] < 48 && a[i -  1] != 45)
+		{
+			a[i] = a[i] - 32;
+		}
+		i++;
+	}
+	return (a);
 }
