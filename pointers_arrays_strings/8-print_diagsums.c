7f31e822f316c78225580de053baebae1816640d
simararustam <simararustam@gmail.com>
2024-03-04 21:13:38 +0000

    Task - 2

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_diagsums - entry point
+ *@a: int
+ *@size: size of array
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += *(a + size * i + i);
+		sum2 += *(a + size * i + size - i - 1);
+	}
+	printf("%d,", sum1);
+	printf("%d\n", sum2);
+}
