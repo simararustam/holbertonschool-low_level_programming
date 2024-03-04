edd3f084c2a7d8084e6ed8ad1481ef7526b3c5f5
simararustam <simararustam@gmail.com>
2024-03-04 12:58:14 +0000

    Task - 2

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_memset - entry point
+ *Return: 0 succes
+ *@s: pntr
+ *@b: pntr
+ *@n: integer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	
+}
