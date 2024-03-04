a8312b1b4a48f49ac2e70ae0037ea1c6483e0776
simararustam <simararustam@gmail.com>
2024-03-04 20:14:00 +0000

    Task - 2

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_strstr - enrty point
+ *@haystack: char
+ *@needle: char
+ *Return: NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+    int i;
+
+    while (*needle)
+    {
+        for (i = 0; haystack[i] != 0; i++)
+        {
+            if (*needle == haystack[i])
+            return(needle);
+        }
+        needle++;
+    }
+    return (NULL);
+}
