a6aaa1eeb47bf0144e82e20e04ad365ae1a0f6e6
simararustam <simararustam@gmail.com>
2024-03-04 14:28:49 +0000

    Task - 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strchr - entry point
+ *@s: ptrn
+ *@c: char
+ *Return: 0 succes
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0, a;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			a = i;
+			while (s[a] != 0)
+			{
+				s
+			}
+		}
+	}
+}
