8a65e58f01756f61682f89f4c286830d5ee04c78
simararustam <simararustam@gmail.com>
2024-03-05 19:07:04 +0000

    Task 2

---
+++ recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *factorial - entry
+ *@n: integer
+ *Return: returned value
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	return (n * factorial(n - 1));
+}
