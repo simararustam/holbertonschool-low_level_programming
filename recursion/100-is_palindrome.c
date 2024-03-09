c0d5d340f63e7d0eafd570f6aeee4db3fb214937
simararustam <simararustam@gmail.com>
2024-03-09 16:11:33 +0000

    Task 2

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -1,12 +1,48 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *is_palindrome - entry point
- *@s: pointer
- *Return: 0 succes
+ *length - find len of s
+ *@s: string
+ *Return: len of string
  */
 
+int length(char *s)
+{
+	if (*s)
+	{
+		s++;
+		return (1 + length(s));
+	}
+	return (0);
+}
+/**
+ *checker - check for ispalindrome
+ *@s: string
+ *@len: len of str
+ *@start: start index of str
+ *Return: 1 if is palindrome , 0 not palindrome
+ */
+int checker(char *s, int len, int start)
+{
+	if (start >= len)
+	{
+		return (1);
+	}
+	if (s[len] == s[start])
+	{
+		return (checker(s, len - 1, start + 1));
+	}
+	return (0);
+}
+/**
+ *is_palindrome - entry point
+ *@s: string
+ *Return: result
+ */
 int is_palindrome(char *s)
 {
+	int len = length(s);
+	int start = 0;
 
+	return (checker(s, len - 1, start));
 }
