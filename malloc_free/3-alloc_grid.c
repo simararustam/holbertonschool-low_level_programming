bf41b8ddf5a9ce8c6140992ec715b761dea631a1
simararustam <simararustam@gmail.com>
2024-03-12 17:32:44 +0000

    Write a function that returns a pointer to a 2 dimensional array of integers.

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	di_arr = malloc(height * sizeof(int));
+	di_arr = malloc(sizeof(int *) * height);
 
 	if (di_arr == NULL)
 		return (NULL);
