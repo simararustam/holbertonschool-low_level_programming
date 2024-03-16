f1314d2910533c7ee938196b0254c77a4da405e2
simararustam <simararustam@gmail.com>
2024-03-16 12:51:38 +0000

    Write a function that concatenates all the arguments of your program.

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ *argstostr - main point
+ *@ac: size of arr
+ *@av: arr
+ *Return: succes
+ */
+char *argstostr(int ac, char **av)
+{
+	char *concat;
+	int i, size = 0, j = 0, k = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		while (av[i][j])
+		{
+			size++;
+			j++;
+		}
+	}
+
+	concat = malloc(sizeof(char) * size + 1);
+	if (concat == NULL)
+		return (NULL);
+
+	i = 0;
+	j = 0;
+	while (i < size)
+	{
+		while (av[j][k])
+		{
+			concat[i] = av[j][k];
+			i++;
+			k++;
+		}
+		j++;
+		k = 0;
+		concat[i] = '\n';
+		i++;
+	}
+	concat[i] = '\0';
+	return (concat);
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -2,4 +2,5 @@
 #define HH
 char *create_array(unsigned int size, char c);
 void simple_print_buffer(char *buffer, unsigned int size);
+char *argstostr(int ac, char **av);
 #endif
