aa8c71cb395be1819e5c13d56a4e0b123cf15776
simararustam <simararustam@gmail.com>
2024-03-18 21:04:10 +0000

    typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,4 +14,13 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * dog_t - typedef for strc dog
+ */
+typedef struct dog
+{
+        char *name;
+        float age;
+        char *owner;
+}dog_t;
 #endif
