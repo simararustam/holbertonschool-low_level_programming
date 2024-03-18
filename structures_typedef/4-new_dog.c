99f13fcefb81177e652337c7183fd8f661d867fc
simararustam <simararustam@gmail.com>
2024-03-18 21:25:14 +0000

    Write a function that creates a new dog.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -6,6 +6,7 @@
  *@name: name
  *@age: age
  *@owner: owner
+ *Return: returned value
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -14,7 +15,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	my_dog = malloc(sizeof(dog_t));
 
 	if (my_dog == NULL)
-		return NULL;
+		return (NULL);
 	my_dog->name = name;
 	my_dog->age = age;
 	my_dog->owner = owner;
