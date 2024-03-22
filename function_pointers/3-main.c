8dd1767377a41fdabfa06b42ba79b0d24163d16d
simararustam <simararustam@gmail.com>
2024-03-22 22:15:56 +0000

    Write a program that performs simple operations.

---
+++ function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main -entry point
+ * @argc: size of arg
+ * @argv: elements of args
+ * Return: 0 succes
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, calc;
+	int (*op)(int, int);
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = get_op_func(argv[2]);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (op == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((op == op_div && num2 == 0) || (op == op_mod && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	calc = op(num1, num2);
+
+	printf("%d\n", calc);
+
+	return (0);
+}
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
 /**
  * op_add - add
@@ -12,6 +13,9 @@ int op_add(int a, int b)
 }
 /**
  * op_sub - sub
+ * @a: a
+ * @b: b
+ * Return: 0
  */
 int op_sub(int a, int b)
 {
@@ -19,6 +23,9 @@ int op_sub(int a, int b)
 }
 /**
  * op_mul - mul
+ * @a: a
+ * @b: b
+ * Return: 0
  */
 int op_mul(int a, int b)
 {
@@ -26,13 +33,16 @@ int op_mul(int a, int b)
 }
 /**
  * op_div - div
+ * @a: a
+ * @b: b
+ * Return: 0.
  */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 /**
- * op_mod
+ * op_mod - mod
  * @a: a
  * @b: b
  * Return: value
