d610a9d7ed58e8d14ad1be3b8dc0c5401a26af34
simararustam <simararustam@gmail.com>
2024-04-02 14:50:21 +0000

    Write a function that adds a new node at the end of a list_t list.

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <sdtlib.h>
+#include "liss.h"
+
+/**
+ *add_node_end - adds a new node at the end of a list_t
+ *@head: start of list
+ *@str: data
+ *Return: the address of the new element
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	
+}
