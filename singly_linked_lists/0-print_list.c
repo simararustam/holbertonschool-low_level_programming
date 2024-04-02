d0cfab5bfc49b5d68e4ad2cdbaab9942648191c2
Simara <115341811+simararustam@users.noreply.github.com>
2024-04-02 12:52:15 +0400

    Update 0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
     for (i = 0; h; i++)
     {
         if (h->str == NULL)
-          printf("[0] (nil)");
+          printf("[0] (nil)\n");
         else
           printf("[%d] %s\n", h->len, h->str);
         h = h->next;
