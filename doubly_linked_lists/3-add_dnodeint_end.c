b96391e09b746719fe2476ba175d05640624450d
Simara <115341811+simararustam@users.noreply.github.com>
2024-04-05 19:06:06 +0400

    Update 3-add_dnodeint_end.c

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,6 +12,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *ptr, *temp;
 
 	ptr = malloc(sizeof(dlistint_t));
+	temp = malloc(sizeof(dlistint_t));
 
 	if (*head == NULL)
 	{
