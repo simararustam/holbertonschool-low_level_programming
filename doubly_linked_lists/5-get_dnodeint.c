1e4aad294ff4cc555e92993182fe6f1f3341086f
simararustam <simararustam@gmail.com>
2024-04-06 06:39:15 +0000

    Write a function that frees a dlistint_t list.

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- *
- *
- *
+ * get_dnodeint_at_index - returns nth node
+ *@head: head of list
+ *@index: nth index of node
+ *Return: nth index of node returned
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	
+
 	if (head == NULL)
 		return (NULL);
 
