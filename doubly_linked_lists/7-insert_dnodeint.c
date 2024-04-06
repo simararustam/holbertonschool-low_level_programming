985781a4cce94e698549e83703b9e80dcbc30dd5
Simara <115341811+simararustam@users.noreply.github.com>
2024-04-06 12:30:25 +0400

    Update 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,34 +11,36 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *prev_node;
-	unsigned int i;
+	dlistint_t *new, *old;
+	unsigned int a;
 
 	if (h == NULL)
 		return (NULL);
+
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	prev_node = *h;
+	old = *h;
 
-	for (i = 0; prev_node != NULL && i < idx; i++)
-		prev_node = prev_node->next;
+	for (a = 0; old != NULL && a < idx; a++)
+		old = old->next;
 
-	if (prev_node == NULL && i == idx)
-		return (add_dnodeint(h, n));
-	else if (prev_node != NULL)
+	if (old == NULL && a == idx)
+		return (add_dnodeint_end(h, n));
+
+	else if (old != NULL)
 	{
 		new = malloc(sizeof(dlistint_t));
+
 		if (new == NULL)
 			return (NULL);
 		new->n = n;
-		prev_node->prev->next = new;
-		new->prev = prev_node->prev;
-		prev_node->prev = new;
-		new->next = prev_node;
+		old->prev->next = new;
+		new->prev = old->prev;
+		old->prev = new;
+		new->next = old;
 
 		return (new);
 	}
-
 	return (NULL);
 }
