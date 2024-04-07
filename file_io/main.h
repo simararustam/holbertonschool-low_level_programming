70950acf0befd6042f619d46b230bff2ccca54e1
Simara <115341811+simararustam@users.noreply.github.com>
2024-04-07 14:27:06 +0400

    Update main.h

--- file_io/main.h
+++ file_io/main.h
@@ -3,24 +3,4 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-/**
- * struct copy_struct - struct for cp
- * @from: from file
- * @to: to file
- * @rd: rd store
- * @wt: write store
- * @from_file: open of from file
- * @to_file: open of to file
- * @buffer: buffer to read to
- */
-typedef struct copy_struct
-{
-	char *from;
-	char *to;
-	ssize_t rd;
-	ssize_t wt;
-	int from_file;
-	int to_file;
-	char *buffer;
-} copy_struct;
 #endif
