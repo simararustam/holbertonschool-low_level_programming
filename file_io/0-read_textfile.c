bb8fb4e9acb8535579ac589bfbb07a61a53afe0e
Simara <115341811+simararustam@users.noreply.github.com>
2024-04-07 14:18:50 +0400

    Update 0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,24 +1,25 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
-
+#include <unistd.h>
+#include <fcntl.h>
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(int ac, char **av)
+  * read_textfile - reads a text file and prints it to the POSIX standard outpu
+  * @filename: name of file
+  * @letters: number of letters
+  * Return:  actual number of letters it could read and print
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t n;
+	char *a = malloc(letters);
+	int fd, l;
 
-    if (ac != 2)
-    {
-        dprintf(2, "Usage: %s filename\n", av[0]);
-        exit(1);
-    }
-    n = read_textfile(av[1], 114);
-    printf("\n(printed chars: %li)\n", n);
-    n = read_textfile(av[1], 1024);
-    printf("\n(printed chars: %li)\n", n);
-    return (0);
+	if (!filename || !a)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	l = read(fd, a, letters);
+	if (l == -1)
+		return (0);
+	if (write(STDOUT_FILENO, a, l) == -1)
+		return (0);
+	close(fd);
+	return (l);
 }
